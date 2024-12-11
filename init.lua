ba80683ac5594e99b78c7ad8effa4d43c24efc50
Jagvir Dehal <jdehal@tesla.com>
2024-12-10 17:28:34 -0800

    Improve minipairs

--- init.lua
+++ init.lua
@@ -876,7 +876,7 @@ require('lazy').setup({
       --   }
       -- }
       require('mini.pairs').setup{
-        modes = { insert = true, command = true, terminal = false },
+        modes = { insert = true, command = false, terminal = false },
         -- skip autopair when next character is one of these
         skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
         -- skip autopair when the cursor is inside these treesitter nodes
